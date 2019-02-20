81053978aa5bec306fd21cd69028fc883c78406c
hayami <hayami.taruto@gmail.com>
2019-02-20 10:29:31 +0900

    [fix] home.html.slim

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,11 +8,17 @@ class StaticPagesController < ApplicationController
   end
 
   def create
-    @card = Card.new
-    @card.title = params[:card][:title]
-    @card.detail = params[:card][:detail]
-    @card.comment = params[:card][:comment]
-    @card.save
-    redirect_to root_path
+    @card = Card.new(card_params)
+    if @card.save
+        redirect_to root_path
+    else
+    end
+  end
+
+
+  private
+  def card_params
+    params.require(:card).permit(:title, :detail, :comment)
+
   end
 end
