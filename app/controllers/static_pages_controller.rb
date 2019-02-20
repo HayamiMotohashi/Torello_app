c3d7079ae0372d93288108b929865cf858bbe860
hayami <hayami.taruto@gmail.com>
2019-02-20 10:39:56 +0900

    [fix] static_pages_controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,15 +10,15 @@ class StaticPagesController < ApplicationController
   def create
     @card = Card.new(card_params)
     if @card.save
-        redirect_to root_path
+      redirect_to root_path
     else
+      render 'new'
     end
   end
 
-
   private
+  
   def card_params
     params.require(:card).permit(:title, :detail, :comment)
-
   end
 end
